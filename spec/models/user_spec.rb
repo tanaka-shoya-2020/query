fc46797dc2ba6f850de643c5c941e7e5bd309b22
tanaka-shoya-2020 <71364105+tanaka-shoya-2020@users.noreply.github.com>
2020-10-22 19:35:30 +0900

    静的コード解析ツールの導入

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -33,35 +33,35 @@ RSpec.describe User, type: :model do
       end
 
       it 'passwordが5文字以下の英数字だと登録できない' do
-        @user.password = "1111a"
+        @user.password = '1111a'
         @user.password_confirmation = @user.password
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+        expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
       end
 
       it 'passwordが6文字以上の英字のみだと登録できない' do
-        @user.password = "aaaaaa"
+        @user.password = 'aaaaaa'
         @user.password_confirmation = @user.password
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password is invalid")
+        expect(@user.errors.full_messages).to include('Password is invalid')
       end
 
       it 'passwordが6文字以上の数字のみだと登録できない' do
-        @user.password = "111111"
+        @user.password = '111111'
         @user.password_confirmation = @user.password
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password is invalid")
+        expect(@user.errors.full_messages).to include('Password is invalid')
       end
 
       it 'password_confirmationが空だと登録できない' do
-        @user.password_confirmation = ""
+        @user.password_confirmation = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
 
       it 'passwordとpassword_confirmationが同じでないと登録できない' do
-        @user.password = "11111a"
-        @user.password_confirmation = "1111111a"
+        @user.password = '11111a'
+        @user.password_confirmation = '1111111a'
         @user.valid?
         expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
