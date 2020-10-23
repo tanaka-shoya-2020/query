0ed074a4f7b3a54ea3e164bf28f2867cbfb873b0
tanaka-shoya-2020 <71364105+tanaka-shoya-2020@users.noreply.github.com>
2020-10-23 09:34:04 +0900

    ルームの単体モデルテストコード

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,9 @@
 class Room < ApplicationRecord
   validates :name, presence: true, length: { maximum: 20},
             uniqueness: true
-
   has_secure_password
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
+  validates :password, presence: true
 end
