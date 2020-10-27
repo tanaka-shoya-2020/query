a8316a7fbe17b7442e77eae07d80583acd0d33f4
tanaka-shoya-2020 <71364105+tanaka-shoya-2020@users.noreply.github.com>
2020-10-27 22:20:57 +0900

    コメントアウトを削除

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,6 @@ module ApplicationHelper
 
   class HTMLwithCoderay < Redcarpet::Render::HTML
     def block_code(code, language)
-      # language = language.split(':')[0]
 
       lang = case language.to_s
              when 'rb'
